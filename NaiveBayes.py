5752ff69d1c2b01d2a346a9a25dc612e1c72c841
Kyle Lewis <kylelewis090@gmail.com>
2018-08-26 13:21:10 -0400

    Works to classify Russian from Normal sets, Isn't operating in a boolean feature way, Also should be shoved in a class implementation

--- NaiveBayes.py
+++ NaiveBayes.py
@@ -4,11 +4,13 @@ import nltk
 
 #from BayesData import BayesData 
 #from Tokenizer import Tokenizer 
+from StopDicts import StopWords, StopPunctuation
 
 import numpy as np 
 import pandas as pd
 from collections import namedtuple 
-
+import operator
+from tqdm import tqdm 
 ############################################
 # Set up datasets for bayes classification # 
 ############################################
@@ -38,14 +40,17 @@ bayesData = BayesData(None, None, None, None)
 # in_reply_to_status_id [15] number			 no 
 
 # For testing purposes. The full dataset has 203k tweets 
-maxRows = 5000
+maxRows = 100000
+fractionForLearning = 0.5
+numSamples = int(maxRows*fractionForLearning)
+
 russianDataRaw = pd.read_csv("data/Known_Russian_Tweets.csv", 
 							 quotechar="\"",
 						     header = 0,
 						     usecols = (7,),# 4, 6, 10, 11, 13),
 						     nrows = maxRows)
 
-print(russianDataRaw)
+# print(russianDataRaw)
 
 # Normal dataset comes with the columns:
 # user_id				[0] no 
@@ -58,10 +63,134 @@ normalDataRaw = pd.read_csv("data/Normal_Tweets.txt",
 							sep = "	",
 							nrows = maxRows)
 
-print(normalDataRaw)
-# print(russianDataRaw.shape[0])
-print(str(russianDataRaw["text"][0]))
-for i in range(russianDataRaw.shape[0]-1):
-	print(nltk.word_tokenize(russianDataRaw["text"][i]))
+normalDataRaw.columns = ['text']
+
+# print(normalDataRaw)
+
+#####################################################
+### Building up counts for words in each category ###
+#####################################################
+
+NormalDataCounts = dict()
+RussianDataCounts = dict()
+
+print("Reading in normal data counts:")
+normalErrors = 0
+for i in tqdm(range(numSamples)):
+	try:
+		words = nltk.word_tokenize(normalDataRaw['text'][i])
+		for word in nltk.word_tokenize(normalDataRaw['text'][i]):
+			if(word.lower() not in StopWords and word.lower() not in StopPunctuation):
+				if word.lower() in NormalDataCounts:
+					NormalDataCounts[word.lower()] += 1
+				else:
+					NormalDataCounts[word.lower()] = 1
+	except TypeError:
+		normalErrors += 1
+
+print("Failed to read " + str(normalErrors) + " lines from normal data")
+print("Reading in Russian data counts:")
+russianErrors = 0
+for i in tqdm(range(numSamples)):
+	try:
+		for word in nltk.word_tokenize(russianDataRaw['text'][i]):
+			if(word.lower() not in StopWords and word.lower() not in StopPunctuation):
+				if word.lower() in RussianDataCounts:
+					RussianDataCounts[word.lower()] += 1
+				else:
+					RussianDataCounts[word.lower()] = 1
+	except TypeError:
+		russianErrors += 1
+
+print("Failed to read " + str(russianErrors) + " lines from normal data")
 
 
+######################
+### Just reporting ###
+######################
+
+print("RussianDataCounts: --------------------------------")
+for key, value in sorted(RussianDataCounts.items(), key=operator.itemgetter(1)):
+	if value > 500:
+		print(key + ' ' + str(value))
+
+print("NormalDataCounts: --------------------------------")
+for key, value in sorted(NormalDataCounts.items(), key=operator.itemgetter(1)):
+	if value > 500:
+		print(key + ' ' + str(value))
+
+
+def LaplaceLikelihood(aWord, aDict, numSamples):
+	if (aWord in aDict):
+		return (aDict[aWord] + 1.0) / (numSamples + 2.0)
+	else:
+		return 1.0 / (numSamples + 2.0)
+
+###################
+### Predictions ###
+###################
+
+numRussSamples = numSamples #int(fractionForLearning*russianDataRaw.shape[0])
+numNormSamples = numSamples #int(fractionForLearning*normalDataRaw.shape[0])
+totalSamples = numRussSamples + numNormSamples
+# Calculate priors up front, they are constant for each prediction:
+RussianPrior = numRussSamples / totalSamples
+NormalPrior = numNormSamples / totalSamples
+
+russianLabel = 1 
+normalLabel = 0
+
+LaplaceFactor = 1
+
+print("Attempting predictions on the remaining normal dataset:")
+normalNumCorrect = 0
+normalNumIncorrect = 0
+normalErrors = 0
+for idx in tqdm(range(numNormSamples, normalDataRaw.shape[0])):
+	RussianLikelihood = 1
+	NormalLikelihood = 1
+	try:
+		for word in nltk.word_tokenize(normalDataRaw['text'][idx]): #TODO store the filtered data when you gather counts so you don't have to double process things
+			lword = word.lower()
+			if(lword not in StopWords and lword not in StopPunctuation):
+				RussianLikelihood *= LaplaceLikelihood(lword, RussianDataCounts, numRussSamples)
+				NormalLikelihood *= LaplaceLikelihood(lword, NormalDataCounts, numNormSamples)
+
+		RussianProb = RussianLikelihood * RussianPrior
+		NormalProb = NormalLikelihood * NormalPrior
+		if RussianProb > NormalProb:
+			normalNumIncorrect += 1 
+		else: 
+			normalNumCorrect += 1
+	except TypeError: 
+		normalErrors += 1
+
+accuracyNormal = (normalNumCorrect) / (normalNumCorrect + normalNumIncorrect)*100.0
+print ("Accuracy in predicting on the normal dataset: " + str(accuracyNormal))
+
+
+print("Attempting predictions on the remaining Russian dataset:")
+russianNumCorrect = 0
+russianNumIncorrect = 0
+normalErrors = 0
+for idx in tqdm(range(numNormSamples, russianDataRaw.shape[0])):
+	RussianLikelihood = 1
+	NormalLikelihood = 1
+	try:
+		for word in nltk.word_tokenize(russianDataRaw['text'][idx]): #TODO store the filtered data when you gather counts so you don't have to double process things
+			lword = word.lower()
+			if(lword not in StopWords and lword not in StopPunctuation):
+				RussianLikelihood *= LaplaceLikelihood(lword, RussianDataCounts, numRussSamples)
+				NormalLikelihood *= LaplaceLikelihood(lword, NormalDataCounts, numNormSamples)
+
+		RussianProb = RussianLikelihood * RussianPrior
+		NormalProb = NormalLikelihood * NormalPrior
+		if RussianProb < NormalProb:
+			russianNumIncorrect += 1 
+		else: 
+			russianNumCorrect += 1
+	except TypeError: 
+		normalErrors += 1
+
+accuracyRussian = (russianNumCorrect) / (russianNumCorrect + russianNumIncorrect)*100.0
+print ("Accuracy in predicting on the russian dataset: " + str(accuracyRussian))
